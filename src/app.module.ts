f1a3b8aa2800a3e549c6e3dac5f795223611cb3c
trdinhlong <dinhlongit24@gmail.com>
2021-01-19 19:58:32 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://storm:admin@cluster0-fydzq.mongodb.net/leads?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
